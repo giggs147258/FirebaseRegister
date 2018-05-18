a6d4cb892920abf347d9141e292adbbded290e35
RTP-10305 <giggs147258@gmail.com>
2018-05-18 11:42:30 +0700

    final

--- app/src/main/java/th/rmutsv/co/th/firebase/MainActivity.java
+++ app/src/main/java/th/rmutsv/co/th/firebase/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -13,14 +14,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     public FirebaseDatabase firebaseDatabase;
-    public DatabaseReference databaseReference;
+    public DatabaseReference databaseReference,databaseReference2,text;
 
     private static final String TAG = "LED Control";
-    public Button Switch;
-    public Integer Value, Value_refer;
+    public Button Switch,Switch2;
+    public Integer Value,Value_refer,Value2,Value_refer2;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         firebaseDatabase = FirebaseDatabase.getInstance();
+
+        /*------------------------------ LED1 ---------------------------------------*/
         databaseReference = firebaseDatabase.getReference("Switch1/LED1");
 
         Switch = (Button) findViewById(R.id.led1);
@@ -59,5 +65,54 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /*------------------------------ LED2 ---------------------------------------*/
+        databaseReference2 = firebaseDatabase.getReference("Switch1/LED2");
+
+        Switch2 = (Button) findViewById(R.id.led2);
+        databaseReference2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Value2 = dataSnapshot.getValue(Integer.class);
+                Log.d(TAG,"Value is" + Value2);
+
+                if (Value2 == 1) {
+                    Switch2.setText("ON");
+                    Value_refer2 = 0;
+                }else {
+                    Switch2.setText("OFF");
+                    Value_refer2 = 1;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
+
+        Switch2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                databaseReference2.setValue(Value_refer2);
+            }
+        });
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        text = firebaseDatabase.getReference();
+        textView = (TextView) findViewById(R.id.textView2);
+        text.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map map = (Map) dataSnapshot.getValue();
+                String index = String.valueOf(map.get("index"));
+                textView.setText(index);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
+
 }
